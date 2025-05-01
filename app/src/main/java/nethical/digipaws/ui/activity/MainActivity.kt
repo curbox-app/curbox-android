725d5260a11f9f7945d8bf0430638dba52bbdd01
Rindo (aka AmixemHello) <68107170+yoann256@users.noreply.github.com>
2025-05-01 19:49:48 +0200

    Update MainActivity.kt

--- app/src/main/java/nethical/digipaws/ui/activity/MainActivity.kt
+++ app/src/main/java/nethical/digipaws/ui/activity/MainActivity.kt
@@ -262,6 +262,8 @@ class MainActivity : AppCompatActivity() {
         // click listener for select lang button
         binding.langSelect.setOnClickListener {
             Toast.makeText(applicationContext, "hey", Toast.LENGTH_SHORT).show()
+            val dialogSelectLang = DialogSelectLangBinding.inflate(layoutInflater)
+            
             val dialog = MaterialAlertDialogBuilder(this)
                          .setView(dialogSelectLang.root)
                          .show()
