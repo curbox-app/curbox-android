ed01e15eb9a5f6a5c752f54913d4552e96eb0a76
Rindo (aka AmixemHello) <68107170+yoann256@users.noreply.github.com>
2025-05-01 19:15:32 +0200

    Replaced & Updated SelectLang.kt

--- app/src/main/java/nethical/digipaws/ui/dialogs/SelectLang.kt
+++ app/src/main/java/nethical/digipaws/ui/dialogs/SelectLang.kt
@@ -1,34 +1,78 @@
-// TODO: Change this and make a layout
-
 package nethical.digipaws.ui.dialogs
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.view.MotionEvent
-import android.widget.ScrollView
-import androidx.fragment.app.DialogFragment
+import android.app.Dialog
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import nethical.digipaws.Constants
+import nethical.digipaws.R
+import nethical.digipaws.databinding.DialogSelectLangBinding
+// import nethical.digipaws.services.GeneralFeaturesService
+// import nethical.digipaws.utils.GrayscaleControl
 import nethical.digipaws.utils.SavedPreferencesLoader
-import nl.joery.timerangepicker.TimeRangePicker
 
-open class SelectLang(val savedPreferencesLoader: SavedPreferencesLoader? = null) :
-    DialogFragment() {
-    fun sendRefreshRequest(action: String) {
-        val intent = Intent(action)
-        context?.sendBroadcast(intent)
-    }
+class SelectLang(
+    savedPreferencesLoader: SavedPreferencesLoader
+) : BaseDialog(savedPreferencesLoader) {
+
+    private lateinit var trackerPreferences: SharedPreferences
+
+    @SuppressLint("ApplySharedPref")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialogSelectLangBinding = DialogSelectLangBinding.inflate(layoutInflater)
+
+        // Load tracker preferences
+        trackerPreferences =
+            requireContext().getSharedPreferences("grayscale", Context.MODE_PRIVATE)
+        val getMode = trackerPreferences.getInt("mode",Constants.GRAYSCALE_MODE_ONLY_SELECTED)
 
 
-    @SuppressLint("ClickableViewAccessibility")
-    fun fixPickerInterceptBug(scrollview: ScrollView, picker: TimeRangePicker) {
-        picker.setOnTouchListener { v, event ->
-            // Disable ScrollView's touch interception when interacting with the picker
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> scrollview.requestDisallowInterceptTouchEvent(true)
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> scrollview.requestDisallowInterceptTouchEvent(
-                    false
-                )
+
+        when(getMode){
+            Constants.GRAYSCALE_MODE_ONLY_SELECTED -> {
+                dialogGrayscaleBinding.blockSelected.isChecked = true
             }
-            v.onTouchEvent(event) // Pass the event to the picker
+            Constants.GRAYSCALE_MODE_ALL -> {
+                dialogGrayscaleBinding.blockAll.isChecked = true
+            }
+            Constants.GRAYSCALE_MODE_ALL_EXCEPT_SELECTED -> {
+                dialogGrayscaleBinding.blockExceptSelected.isChecked = true
+            }
         }
+
+        // Build and display dialog
+        return MaterialAlertDialogBuilder(requireContext())
+            .setView(dialogGrayscaleBinding.root)
+            .setCancelable(false)
+            .setPositiveButton(getString(R.string.apply)) { dialog, _ ->
+                when(dialogGrayscaleBinding.modeType.checkedRadioButtonId){
+                    dialogGrayscaleBinding.blockAll.id -> {
+                        trackerPreferences.edit().putInt("mode",Constants.GRAYSCALE_MODE_ALL).commit()
+                        val grayscaleControl = GrayscaleControl()
+                        grayscaleControl.enableGrayscale()
+                    }
+                    dialogGrayscaleBinding.turnOff.id -> {
+                        trackerPreferences.edit().putInt("mode",Constants.GRAYSCALE_MODE_OFF).commit()
+                        val grayscaleControl = GrayscaleControl()
+                        grayscaleControl.disableGrayscale()
+                    }
+                    dialogGrayscaleBinding.blockSelected.id -> {
+                        trackerPreferences.edit().putInt("mode",Constants.GRAYSCALE_MODE_ONLY_SELECTED).commit()
+                    }
+                    dialogGrayscaleBinding.blockExceptSelected.id -> {
+                        trackerPreferences.edit().putInt("mode",Constants.GRAYSCALE_MODE_ALL_EXCEPT_SELECTED).commit()
+                    }
+
+                }
+                // Send broadcast to refresh UsageTrackingService
+                sendRefreshRequest(GeneralFeaturesService.INTENT_ACTION_REFRESH_GRAYSCALE)
+                dialog.dismiss()
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
     }
-    }
+}
